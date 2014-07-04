ce6b32cce0ecf73a6fc410ce5341e488bf5c8a29
Luke San Antonio <lukesanantonio@gmail.com>
2014-07-04 18:36:16 -0400

    Moved the other types out of the gen folder, finally.

---
+++ types/scss.py
@@ -0,0 +1,9 @@
+# Gen v0
+# A generic, JSON-based asset pipeline for heterogeneous setups and
+# unusual configurations.
+#
+# This is free and unencumbered software released into the public domain.
+# For more information, please refer to <http://unlicense.org/>
+
+def run(action, asset_dir, out_dir):
+    pass
