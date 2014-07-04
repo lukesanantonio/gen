369ef7454c4c6c2b5ef5b6e5639925ca6ae22cac
Luke San Antonio <lukesanantonio@gmail.com>
2014-07-04 16:38:30 -0400

    Simplified the gen.py file. It now only searchs for an assets.json file in the project root.

--- gen.py
+++ gen.py
@@ -9,60 +9,28 @@ import os
 import json
 import imp
 
-def find_asset_files(asset_dir, asset_filename = "asset.json"):
-    """Return a list of relative paths to all asset json files.
-
-    Keyword Arguments:
-    asset_dir -- root directory to be searched.
-    """
-
-    # If we aren't even given a directory, bail.
-    if not os.path.isdir(asset_dir):
-        return []
-
-    files = []
-
-    contents = os.listdir(asset_dir);
-    if asset_filename not in contents:
-        for dirname in contents:
-            next_dir = os.path.join(asset_dir, dirname)
-            files.extend(find_asset_files(next_dir))
-    else:
-        # The asset.json or equivalent lives in this directory.
-        # Add whatever it's called to the list.
-        files.append(os.path.join(asset_dir, asset_filename))
-
-    return files
-
 if __name__ == '__main__':
-    # Root is absolute, as are the rest.
+    # Enter the directory of this script assumed to be the project root.
     root = os.path.abspath(os.path.dirname(__file__))
-    assets = os.path.join(root, 'assets')
-    dist = os.path.join(root, 'dist')
-    types = os.path.join(root, 'types')
-
     os.chdir(root)
 
-    for asset_json in find_asset_files(os.path.relpath(assets)):
+    # Figure out some other useful paths
+    dist = os.path.join(root, 'dist')
 
-        # Extract the action object!
-        action = json.load(open(asset_json))
+    # Parse the assets.json file.
+    assets_json = json.load(open('assets.json'))
 
-        # Get the relative path to the asset folder containing the asset.json.
-        this_asset = os.path.dirname(asset_json)
-
-        # Figure out the distribution directory for this asset. The default is
-        # to use the name of the folder containing the asset.json file.
-        # However, if output_dir is specified in the action object, that is
-        # preferred.
-        this_dist = os.path.join(dist, os.path.relpath(this_asset, assets))
-        if 'output_dir' in action:
-            this_dist = os.path.join(dist, action['output_dir'])
-
-        # Extract the python module to load:
-        module_tuple = imp.find_module(action['type'], [types])
-        action_module = imp.load_module(action['type'], module_tuple[0],
-                                                        module_tuple[1],
-                                                        module_tuple[2])
-
-        action_module.run(action, this_asset, this_dist)
+    # For each asset
+    for asset in assets_json:
+        # Load our extension
+        try:
+            module = imp.find_module(asset['type'], ['types/'])
+            action = imp.load_module(asset['type'], module[0],
+                                     module[1], module[2])
+        except ImportError as i:
+            print("Invalid or unknown type: '" + asset['type'] + "'")
+            continue
+        # Find the dist path
+        this_dist = os.path.join(dist, asset.get('dist', asset['root']))
+        # Run the action
+        action.run(asset, asset['root'], this_dist)
